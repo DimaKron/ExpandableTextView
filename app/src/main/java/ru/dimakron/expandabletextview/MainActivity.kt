409ce7dc2accbca1d45fbae26c57eaf57102827b
vladislav <cool.extro5@yandex.ru>
2020-10-05 13:00:55 +0300

    lorem ipsum

--- app/src/main/java/ru/dimakron/expandabletextview/MainActivity.kt
+++ app/src/main/java/ru/dimakron/expandabletextview/MainActivity.kt
@@ -1,11 +1,14 @@
 package ru.dimakron.expandabletextview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        expandableTextView.text = getString(R.string.lorem_ipsum)
     }
 }
